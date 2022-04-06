a1be752e2eb5b2a4f0d7c6ef31c4a97527f7e05d
MaxBelyakov <belyakovdevelopment@gmail.com>
2022-04-06 18:32:30 +0300

    v.1.6

--- Assets/Scripts/Forklift/Forklift.cs
+++ Assets/Scripts/Forklift/Forklift.cs
@@ -1,44 +1,42 @@
-using System;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class Forklift : MonoBehaviour
 {
     public GameObject shifter;      // Forklift shifter
     public GameObject lift;         // Forklift lift
 
-    //private bool lifting;
-
     private HingeJoint hinge;
 
     //angle threshold to trigger if we reached limit
     public float angleBetweenThreshold = 1f;
+    private float angleWithMinLimit;
+    private float angleWithMaxLimit;
 
     private bool limit;
 
     void Awake()
     {
         hinge = shifter.GetComponent<HingeJoint>();
+        angleWithMinLimit = shifter.transform.eulerAngles.x + hinge.limits.min;
+        angleWithMaxLimit = shifter.transform.eulerAngles.x + hinge.limits.max;
     }
 
     void FixedUpdate()
     {
-        float angleWithMinLimit = Mathf.Abs(hinge.angle - hinge.limits.min);
-        float angleWithMaxLimit = Mathf.Abs(hinge.angle - hinge.limits.max);
-
+        print(angleWithMinLimit + "/" + angleWithMaxLimit + "/" + shifter.transform.eulerAngles.x);
         //Reached Max
-        if(angleWithMinLimit < angleBetweenThreshold)
+        if(shifter.transform.eulerAngles.x - angleWithMinLimit < angleBetweenThreshold)
         {
+            print("min");
             if (!limit)
                 lift.transform.Translate(transform.up*1f*Time.deltaTime);
-                //lift.GetComponent<Rigidbody>().MovePosition(lift.GetComponent<Rigidbody>().position + transform.up*1f*Time.deltaTime);
         }
         //Reached Min
-        else if (angleWithMaxLimit < angleBetweenThreshold)
+        else if (angleWithMaxLimit - shifter.transform.eulerAngles.x < angleBetweenThreshold)
         {
+            print("max");
             if (!limit)
                 lift.transform.Translate(transform.up*-1f*Time.deltaTime);
-                //lift.GetComponent<Rigidbody>().MovePosition(lift.GetComponent<Rigidbody>().position + transform.up*-1f*Time.deltaTime);
         }
         //No Limit reached
         else
@@ -46,11 +44,13 @@ public class Forklift : MonoBehaviour
 
         if (!limit && lift.transform.localPosition.y < 0.636f)
         {
+            print("limit min");
             lift.transform.localPosition = new Vector3(lift.transform.localPosition.x, 0.636f, lift.transform.localPosition.z);
             limit = true;
         }
         if (!limit && lift.transform.localPosition.y > 2f)
         {
+            print("limit max");
             lift.transform.localPosition = new Vector3(lift.transform.localPosition.x, 2f, lift.transform.localPosition.z);
             limit = true;
         }
